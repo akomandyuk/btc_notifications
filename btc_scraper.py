37f909031a32fa36fb2e7c1e9c43c2173b8da559
Artyom Komandyuk <artyomkomandyuk@gmail.com>
2022-02-11 22:59:28 +0300

    scrapes btc price

---
+++ btc_scraper.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from playsound import playsound
+
+
+def scrape():
+    url = 'https://coinmarketcap.com/currencies/bitcoin/'
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    current_price_element = soup.body.find(class_='priceValue')
+    return current_price_element.text.replace('$', '').replace(',', '')
+
+
+target_price = 45000
+while True:
+    time.sleep(5)
+    current_price = float(scrape())
+
+    if target_price >= current_price:
+        playsound('audio.wav')
